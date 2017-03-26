4d30e1ee35de20b2776cbf02f25e634562731feb
Shu <1129220400@qq.com>
2017-03-26 10:13:57 +0800

    控制器助手类

--- src/main/java/cn/smart/helper/ControllerHelper.java
+++ src/main/java/cn/smart/helper/ControllerHelper.java
@@ -1,8 +1,57 @@
 package cn.smart.helper;
 
+import cn.smart.annotation.Action;
+import cn.smart.bean.Handler;
+import cn.smart.bean.Request;
+import cn.smart.util.ArrayUtil;
+import cn.smart.util.CollectionUtil;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by shu on 2017/3/24.
  */
 public class ControllerHelper {
+    private static final Map<Request, Handler> ACTION_MAP  = new HashMap<>();
 
+    static {
+        Set<Class<?>> controllerClassSet = ClassHelper.getConClsSet();
+
+        if (CollectionUtil.isNotEmpty(controllerClassSet)) {
+            for (Class<?> controllerClass : controllerClassSet) {
+                 Method[] methods = controllerClass.getDeclaredMethods();
+                if (ArrayUtil.isNotEmpty(methods)) {
+                    for (Method method : methods) {
+                        if (method.isAnnotationPresent(Action.class)) {
+                            //从Action注解中获取URL映射规则
+                            Action action = method.getAnnotation(Action.class);
+                            String mapping = action.value();
+
+                            //验证URL映射规则
+                            if (mapping.matches("\\w+:/\\w*")) {
+                                String[] array = mapping.split(":");
+                                if (ArrayUtil.isNotEmpty(array) && array.length == 2) {
+                                    //获取请求方法与请求路径
+                                    String requestMethod = array[0];
+                                    String requestPath = array[1];
+                                    Request request = new Request(requestMethod, requestPath);
+                                    Handler handler = new Handler(controllerClass, method);
+                                    //初始化
+                                    ACTION_MAP.put(request, handler);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //获取Handler
+    public static Handler getHandler(String requestMethod, String requestPath) {
+        Request request = new Request(requestMethod, requestPath);
+        return ACTION_MAP.get(request);
+    }
 }
