1ccff17f30ac205d4d2b4a4f2934d8d3f5b7cfe7
Shu <1129220400@qq.com>
2017-06-26 20:21:47 +0800

    类加载

--- src/main/java/cn/smart/HelperLoader.java
+++ src/main/java/cn/smart/HelperLoader.java
@@ -1,20 +1,16 @@
 package cn.smart;
 
-import cn.smart.helper.BeanHelper;
-import cn.smart.helper.ClassHelper;
-import cn.smart.helper.ControllerHelper;
-import cn.smart.helper.IocHelper;
+import cn.smart.helper.*;
 import cn.smart.util.ClassUtil;
 
-/**
- * Created by shu on 2017/3/26.
- */
 //初始化框架
 public final class HelperLoader {
     public static void init() {
         Class<?>[] classes = {
                 ClassHelper.class,
                 BeanHelper.class,
+                //AopHelper要在IocHelper之前加载,因为需要通过AopHelper获取代理对象
+                AopHelper.class,
                 IocHelper.class,
                 ControllerHelper.class
         };
